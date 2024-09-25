3970e360fed2033e777448e42d5a7d363ef758e5
Matthew Whited <mwhited@eliassen.com>
2024-09-25 14:39:03 -0400

    fix doc scan config and enable apache tika

--- down.bat
+++ down.bat
@@ -2,4 +2,5 @@ docker compose ^
 --project-name openwebui ^
 --file docker-compose.yaml ^
 --file docker-compose.searxng.yaml ^
+--file docker-compose.apache-tika.yaml ^
 down
--- up.bat
+++ up.bat
@@ -2,5 +2,6 @@ docker compose ^
 --project-name openwebui ^
 --file docker-compose.yaml ^
 --file docker-compose.searxng.yaml ^
+--file docker-compose.apache-tika.yaml ^
 up ^
 --detach 
